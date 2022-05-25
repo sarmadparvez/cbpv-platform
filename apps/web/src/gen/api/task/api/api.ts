040785ee2cf03f1f5e172aeb742b06b7034a7b42
Sarmad Parvez <sarmadparvez92@gmail.com>
2022-05-25 02:17:43 +0200

    chore: add client side generated libraries to gitignore

--- apps/web/src/gen/api/task/api/api.ts
+++
@@ -1,9 +0,0 @@
-export * from './feedbacks.service';
-import { FeedbacksService } from './feedbacks.service';
-export * from './iAM.service';
-import { IAMService } from './iAM.service';
-export * from './projects.service';
-import { ProjectsService } from './projects.service';
-export * from './tasks.service';
-import { TasksService } from './tasks.service';
-export const APIS = [FeedbacksService, IAMService, ProjectsService, TasksService];
