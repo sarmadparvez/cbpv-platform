e162a89798becc791f80fb8dd08391d3413811b3
Sarmad Parvez <sarmadparvez92@gmail.com>
2022-05-30 04:52:00 +0200

    access kubernetes cluster with reverse proxy

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  adminServiceUrl: 'https://upb-cbpv-admin.herokuapp.com',
-  taskServiceUrl: 'https://upb-cbpv-task.herokuapp.com',
+  adminServiceUrl: 'https://upb-admin.herokuapp.com',
+  taskServiceUrl: 'https://upb-task.herokuapp.com',
 };
